dd4c4ee6c3093dbb559dc551119dac273c673b0b
lutkin <lutkin@iut.univ-paris8.fr>
2019-05-14 11:34:34 +0200

    Nouvelle map et nouveau tileset

--- Ressources/Maps/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.05.08" name="tileset2" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="tileset2.png" width="16" height="16"/>
-</tileset>
