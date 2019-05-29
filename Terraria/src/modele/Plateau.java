5f9ffe9cb80b67c9fac9d6b2db27319ec5d05007
Théo Beaurain <tbeaurain@iut.univ-paris8.fr>
2019-05-29 09:42:42 +0200

    Version qui marche

---
+++ Terraria/src/modele/Plateau.java
@@ -0,0 +1,21 @@
+package modele;
+
+public class Plateau {
+
+	private int[][] plateau;
+	private BuilderPlateau bp = new BuilderPlateau();
+	private String url = "Ressources/Maps/mapTest.csv";
+	
+	public Plateau() {
+		this.bp.lireFichier(this.url);
+		this.plateau = bp.getPlateau();
+	}
+	
+	public int casePlateau(Coordonnees coo) {
+		return this.plateau[coo.getCoordY() / 16][coo.getCoordX() / 16];
+	}
+	
+	public int[][] getPlateau() {
+		return this.plateau;
+	}
+}
